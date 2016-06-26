c63af92b461c216566b679f3d996977e881db1a4
André Pinheiro <andrepdpinheiro@gmail.com>
2016-06-26 14:51:45 +0100

    Create 171. Excel Sheet Column Number.rb

---
+++ 171. Excel Sheet Column Number.rb
@@ -0,0 +1,14 @@
+# @param {String} s
+# @return {Integer}
+def title_to_number(s)
+  return 0 if s.empty?
+  return s[0].ord - 64 if s.length == 1
+
+
+  total = 0
+  s = s.chars
+  for i in (s.length - 1).downto(0)
+    total += (s[i].ord - 64) * (26 ** (s.length - 1 - i))
+  end
+  total
+end
