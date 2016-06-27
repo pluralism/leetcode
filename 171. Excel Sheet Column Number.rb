83164bdcc7386bdf64056feb1c840936f85f107c
André Pinheiro <andrepdpinheiro@gmail.com>
2016-06-27 19:45:41 +0100

    Update 171. Excel Sheet Column Number.rb

--- 171. Excel Sheet Column Number.rb
+++ 171. Excel Sheet Column Number.rb
@@ -1,3 +1,16 @@
+#Given a column title as appear in an Excel sheet, return its corresponding column number.
+#For example:
+
+  #A -> 1
+  #B -> 2
+  #C -> 3
+  #...
+  #Z -> 26
+  #AA -> 27
+  #AB -> 28 
+
+
+
 # @param {String} s
 # @return {Integer}
 def title_to_number(s)
