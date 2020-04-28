a8ea99d309db5adb152d608af55adb227e916bdb
André Pinheiro <andrepdpinheiro@gmail.com>
2020-04-28 20:15:32 +0100

    Update SearchInSortedArray.java

--- java/SearchInSortedArray.java
+++ java/SearchInSortedArray.java
@@ -4,20 +4,11 @@ class Solution {
     }
 
     private int helper(int[] nums, int target, int l, int r) {
-        if (l > r) {
-            return -1;
-        }
-
+        if (l > r) return -1;
         int mid = (l + r) / 2;
-        if (nums[mid] == target) {
-            return mid;
-        }
-
-        if (nums[mid] >= nums[l]) {
-            if (target >= nums[l] && target <= nums[mid]) {
-                return helper(nums, target, l, mid - 1);
-            }
-        } else if (target < nums[mid] || target > nums[r]) {
+        if (nums[mid] == target) return mid;
+        if ((nums[mid] >= nums[l] && target >= nums[l] && target <= nums[mid]) ||
+                (nums[mid] < nums[l] && (target < nums[mid] || target > nums[r]))) {
             return helper(nums, target, l, mid - 1);
         }
         return helper(nums, target, mid + 1, r);
