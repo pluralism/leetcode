24be02db56391a0ab4861c8892273bf8eb9e2ff1
André Pinheiro <andrepdpinheiro@gmail.com>
2020-05-24 15:37:06 +0100

    Removed size argument

--- js/78.js
+++ js/78.js
@@ -4,14 +4,14 @@
  */
 var subsets = function(nums) {
     const result = [[]];
-    helper(nums, 0, 1, [], result);
+    helper(nums, 0, [], result);
     return result;
 };
 
-function helper(array, index, size, temp, result) {
+function helper(array, index, temp, result) {
     for (let i = index; i < array.length; i++) {
         const subset = [...temp, array[i]];
         result.push(subset);
-        helper(array, i + 1, size, subset, result);
+        helper(array, i + 1, subset, result);
     }
 }
